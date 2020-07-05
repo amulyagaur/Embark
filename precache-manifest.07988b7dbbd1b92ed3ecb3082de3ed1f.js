d72ad0c4e24582a05e8040f6d5e454efe7161de0
amulya <amulyagaur111@gmail.com>
2020-07-05 21:03:05 +0530

    Updates

---
+++ precache-manifest.07988b7dbbd1b92ed3ecb3082de3ed1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef9c9fa4a96ae536b78d2032172e19c8",
+    "url": "/Embark/index.html"
+  },
+  {
+    "revision": "3de5b3f14f0a8af0ea92",
+    "url": "/Embark/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e3716806cb03d4451044",
+    "url": "/Embark/static/js/2.833630fc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Embark/static/js/2.833630fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3de5b3f14f0a8af0ea92",
+    "url": "/Embark/static/js/main.1cf39027.chunk.js"
+  },
+  {
+    "revision": "446ec16c607a7644fecf",
+    "url": "/Embark/static/js/runtime-main.742c7dc2.js"
+  }
+]);
