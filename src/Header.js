c20dc8d3f314072c9f88dce2a151d0b08a532804
amulya <amulyagaur111@gmail.com>
2020-07-06 12:14:07 +0530

    fixing header

--- src/Header.js
+++ src/Header.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import {AppBar, Toolbar, Grid} from '@material-ui/core';
 import logo from './images/logo.png'
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles((theme) => ({
+  imgCs:{
+      length:'100%',
+      width:'100%'
+  }
+}));
 function Header(props) {
-
+  const classes = useStyles();
         return(
             <AppBar 
                 color="default" position="relative">
                 <Grid container>
-                  <Grid item xs={3} sm = {4} lg={5}></Grid>
+                  <Grid item xs={2} sm = {3} lg={4}></Grid>
                   <Grid item xs={8} sm = {6} lg={4}>
                 <Toolbar>
-                <img src={logo} alt="logo"/>
+                <img src={logo} alt="logo" className={classes.imgCs}/>
                 </Toolbar>
                 </Grid>
-                <Grid item xs={1} sm = {2} lg={3}></Grid>
+                <Grid item xs={2} sm = {3} lg={4}></Grid>
                 </Grid>
             </AppBar>
         )
